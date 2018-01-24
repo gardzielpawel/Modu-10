f84062e6c4ba9b3fb8488ae35ffd503c8f95f99b
Michał Gacek <gacek.michael@outlook.com>
2018-01-24 17:02:32 +0100

    Fixes

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
 $(function() {
   var carouselList = $("#carousel ul");
-  setInterval(carouselList, 3000 );
-  carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-});
 
-var firstItem = carouselList.find("li:first");
-var lastItem = carouselList.find("li:last");
-lastItem.after(firstItem);
-carouselList.css({marginLeft:0});
+  setInterval(function() {
+    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+  }, 3000);  
+
+  function moveFirstSlide() {
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+  }
+});
